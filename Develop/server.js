6536a1aeeb8d10829bc8658ead30e6c0d5e58208
Bentura Peralez <bperalez001@gmail.com>
2021-11-12 22:07:36 -0600

    updates

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,44 @@
-const express = require('express');
-const path = require('path');
+const fs = require("fs");
+
+const express = require("express");
+const path = require("path");
+const { response } = require("express");
+const { send } = require("process");
 
 const app = express();
 const PORT = 3001;
 
-app.get('/notes', (req, res) => res.sendFile(path.join(_dirname, "./public/notes.html")))
+app.use (express.static("public"))
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/send.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/paths.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+app.get("/send", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/send.html"))
 );
 
-//`GET /notes` should return the `notes.html` file.
+app.get("/paths", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/paths.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "./db/db.json"),
+    "utf8",
+    (err, jsonString) => {
+      if (err) {
+        console.log("File read failed:", err);
+        res.send("you cannot read the file");
+      }
+      res.json(JSON.parse(jsonString));
+    }
+  );
+});
+app.listen(PORT);
+
+//The following HTML routes should be created: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
